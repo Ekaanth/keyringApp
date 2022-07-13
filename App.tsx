dc274943b33a06643daef2f9209810ab2bd1825a
Abhishek <abhishek@litentry.com>
2022-07-13 11:10:59 +0200

    indented app

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './src/HomeScreen';
 import GenerateSeedScreen from './src/GenerateSeedScreen';
 import useWasmInit from './hooks/useWasmInit';
 import ChainInfoScreen from './src/ChainInfoScreen';
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   const isWasmInit = useWasmInit();
@@ -17,13 +17,19 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
-       <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Polkadot Keyring Samples" component={HomeScreen}/>
-        <Stack.Screen name="Generate Seeds" component={GenerateSeedScreen}/>
-        <Stack.Screen name="Chain Info" component={ChainInfoScreen}/>
-      </Stack.Navigator>
-      </NavigationContainer>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Polkadot Keyring Samples"
+              component={HomeScreen}
+            />
+            <Stack.Screen
+              name="Generate Seeds"
+              component={GenerateSeedScreen}
+            />
+            <Stack.Screen name="Chain Info" component={ChainInfoScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
       </SafeAreaProvider>
     );
   }
