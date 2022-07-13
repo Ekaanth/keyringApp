6ba90bcb99fc0e812b112f26e22e3b7b8e89f62d
Abhishek <abhishek@litentry.com>
2022-07-13 11:11:52 +0200

    mnemonicGenerate screen update

--- src/GenerateSeedScreen.tsx
+++ src/GenerateSeedScreen.tsx
@@ -1,13 +1,78 @@
-import React from 'react';
-import { View , Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Button} from 'react-native';
+import {Keyring} from '@polkadot/keyring';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
+export default function GenerateSeedScreen({route}: any) {
+  const {mnemonic} = route.params;
+  const keyring = new Keyring({type: 'sr25519', ss58Format: 2});
+  const [address, setAddress] = useState('');
+  const getMnemonicAddress = () => {
+    const pair = keyring.addFromUri(mnemonic, {name: 'first pair'}, 'ed25519');
+    setAddress(pair.address);
+  };
 
-export default function GenerateSeedScreen() {
-return (
-    <View>
-      <Text>
-        GenerateSeedScreen
-      </Text>
-    </View>
+  return (
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <View style={styles.containerBody}></View>
+        <View style={styles.seedContainer}>
+          {mnemonic ? (
+            <>
+              <Text style={styles.seed}> {mnemonic} </Text>
+              <View style={styles.button}>
+                <Button title="Mnemonic Address" onPress={getMnemonicAddress} />
+              </View>
+            </>
+          ) : null}
+        </View>
+        {address ? (
+          <>
+            <View style={styles.containerBody}>
+              <Text>Address</Text>
+              <Text style={styles.address}>{address}</Text>
+            </View>
+          </>
+        ) : null}
+      </View>
+    </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  containerBody: {
+    paddingTop: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  seedContainer: {
+    shadowOpacity: 0.25,
+    elevation: 5,
+    paddingVertical: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  seed: {
+    borderStyle: 'dashed',
+    borderWidth: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 5,
+    fontSize: 16,
+    fontWeight: 'bold',
+    justifyContent: 'center',
+    textAlign: 'center',
+  },
+
+  address: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  button: {
+    paddingTop: 10,
+  },
+});
--- src/HomeScreen.tsx
+++ src/HomeScreen.tsx
@@ -1,28 +1,42 @@
 import React from 'react';
-import { FlatList,SafeAreaView, StyleSheet} from 'react-native';
+import {FlatList, SafeAreaView, StyleSheet} from 'react-native';
 import PolkaItem from './components/polkaItem';
+import {mnemonicGenerate} from '@polkadot/util-crypto';
 
 const DATA = [
   {
     id: '1',
     title: 'Generate Seed phrase',
-    screen: 'Generate Seeds'
+    navigationFunction: 'navigateToSeed',
   },
   {
     id: '2',
     title: 'Chain info',
-    screen: 'Chain Info'
+    navigationFunction: 'navigateToChainInfo',
   },
 ];
 
 export default function HomeScreen({navigation}: any) {
+  function renderItems(itemData: any) {
+    const navigateTo = (item: any) => {
+      console.log(item);
+      switch (item) {
+        case 'navigateToSeed':
+          const mnemonic = mnemonicGenerate();
+          navigation.navigate('Generate Seeds', {mnemonic});
+          break;
+        case 'navigateToChainInfo':
+          navigation.navigate('Chain Info');
+          break;
+      }
+    };
 
-
-function renderItems(itemData: any) {
-  const onNavigation =() => {
-    navigation.navigate(itemData.item.screen)
-  }
-  return <PolkaItem title={itemData.item.title} onPress={onNavigation} />;
+    return (
+      <PolkaItem
+        title={itemData.item.title}
+        onPress={() => navigateTo(itemData.item.navigationFunction)}
+      />
+    );
   }
 
   return (
@@ -36,6 +50,12 @@ function renderItems(itemData: any) {
   );
 }
 
+// <SafeAreaView style={styles.container}>
+//   <View></View>
+//   <PolkaItem title={"Generate Seed phrase"} onPress={navigateToSeed} />
+//   <PolkaItem title={"Chain Info"} onPress={navigateToChainInfo} />
+// </SafeAreaView>
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -48,9 +68,9 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 22,
-    textAlign: 'center'
+    textAlign: 'center',
   },
-  button:{
+  button: {
     flex: 1,
-  }
+  },
 });
