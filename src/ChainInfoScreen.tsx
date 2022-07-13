4a2a5d544ffaa84c40750736d175fe25b5d43e6d
Abhishek <abhishek@litentry.com>
2022-07-13 11:11:39 +0200

    ChainInfoScreen indent

--- src/ChainInfoScreen.tsx
+++ src/ChainInfoScreen.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { View , Text} from 'react-native';
-
+import {View, Text} from 'react-native';
 
 export default function ChainInfoScreen() {
-return (
+  return (
     <View>
-      <Text>
-        ChainInfoScreen
-      </Text>
+      <Text>ChainInfoScreen</Text>
     </View>
   );
 }
