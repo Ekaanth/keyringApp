ade7cc279414d8a8ed65104600ce98ccbbb68e4e
Abhishek <abhishek@litentry.com>
2022-07-26 17:26:40 +0200

    RegistrarsScreen display registrars

--- src/RegistrarsScreen.tsx
+++ src/RegistrarsScreen.tsx
@@ -1,20 +1,68 @@
-import React, {useMemo} from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, FlatList, StyleSheet} from 'react-native';
 import useRpcConnect from './hooks/useRpcConnect';
+
+type registrars = {
+  account: string;
+  fee: string;
+  fields: Array<string>[];
+};
 
 export default function RegistrarsScreen() {
   const api = useRpcConnect();
-
-  const register = useMemo(() => {
-    const registrars = JSON.stringify(api?.query.identity?.registrars);
-    return registrars;
+  const [registrars, setRegistrars] = useState<registrars[]>();
+  useEffect(() => {
+    if (api) {
+      api?.query.identity?.registrars().then(reg => {
+        setRegistrars(
+          reg
+            .map(r => r.unwrap())
+            .map((r: {account: {toString: () => any}}) => {
+              return {
+                account: r.account.toString(),
+              };
+            }),
+        );
+      });
+    }
   }, [api]);
 
-  console.log(register);
+  if (!registrars) {
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
 
   return (
-    <View>
-      <Text>RegistrarsScreen</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={registrars}
+        keyExtractor={item => item.account}
+        renderItem={({item}) => (
+          <View>
+            <Text>{item.account}</Text>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <View>
+            <Text style={styles.item}>There are no registrars</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22,
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    color: 'black',
+  },
+});
