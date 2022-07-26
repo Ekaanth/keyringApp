afec1bdcae3be3d7105bd89cd1a6e702a9f564d4
Abhishek <abhishek@litentry.com>
2022-07-26 17:50:28 +0200

    chaininfo screen

--- src/ChainInfoScreen.tsx
+++ src/ChainInfoScreen.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
-import { View , Text} from 'react-native';
-
+import React, {useEffect, useState} from 'react';
+import {View, Text} from 'react-native';
+import useRpcConnect from './hooks/useRpcConnect';
 
 export default function ChainInfoScreen() {
-return (
+  const api = useRpcConnect();
+  const [network, setNetwork] = useState('-');
+  const [version, setVersion] = useState('-');
+  useEffect(() => {
+    if (api) {
+      setNetwork(api?.runtimeChain.toString());
+      setVersion(api?.runtimeVersion.transactionVersion.toString());
+    }
+  }, [api]);
+
+  if (!network || !version) {
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
+  return (
     <View>
-      <Text>
-        ChainInfoScreen
-      </Text>
+      <Text>Network Name: {network}</Text>
+      <Text>Network Version: {version}</Text>
     </View>
   );
 }
