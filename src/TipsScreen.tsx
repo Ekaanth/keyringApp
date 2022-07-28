f32947e6c1d97928a9d306e8a03fdc7689c1bbca
Abhishek <abhishek@litentry.com>
2022-07-28 18:32:33 +0200

    tips screen UI

--- src/TipsScreen.tsx
+++ src/TipsScreen.tsx
@@ -1,20 +1,92 @@
-import React, {useMemo} from 'react';
-import {Text, View} from 'react-native';
+import React from 'react';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
 import useRpcConnect from './hooks/useRpcConnect';
+import useTips from './hooks/useTips';
+import {formatBalance} from './service/chainServices';
 
 export default function TipsScreen() {
   const api = useRpcConnect();
+  const {tips, loading} = useTips();
 
-  const tip = useMemo(async () => {
-    const tips = JSON.stringify(api?.query.tips.tips);
-    return tips;
-  }, [api]);
-
-  console.log(tip);
+  if (loading) {
+    return (
+      <View>
+        <Text style={styles.item}>Loading...</Text>
+      </View>
+    );
+  }
 
   return (
-    <View>
-      <Text>TipsScreen</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={tips}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => (
+          <View style={styles.tipsContainer}>
+            <View style={styles.row}>
+              <Text style={styles.bold}>Proposed By: </Text>
+              <Text>{item.finder.address}</Text>
+            </View>
+            <View style={styles.row}>
+              <Text style={styles.bold}>Deposit: </Text>
+              <Text>{formatBalance(api, item.deposit.toString())}</Text>
+            </View>
+            <View style={styles.row}>
+              <Text style={styles.bold}>Number of Tippers: </Text>
+              <Text>{item.tippers.length}</Text>
+            </View>
+            <View>
+              <Text style={styles.reason}>Reason</Text>
+              <Text>{item.reason}</Text>
+            </View>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <View>
+            <Text style={styles.item}>There are no active tips</Text>
+          </View>
+        )}
+        ListHeaderComponent={() => (
+          <View>
+            <Text style={styles.header}>Tips</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22,
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+  },
+  header: {
+    textAlign: 'center',
+    fontSize: 22,
+  },
+  tipsContainer: {
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    margin: 10,
+  },
+  row: {
+    flex: 0,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  reason: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+});
