e3b651b183970421115ef4aeec6cc75e92d4f1f7
Abhishek <abhishek@litentry.com>
2022-07-28 17:27:50 +0200

    getBlockTime function added

--- src/service/chainServices.ts
+++ src/service/chainServices.ts
@@ -1,10 +1,11 @@
 import {Registry} from '@polkadot/types/types';
-import {formatBalance as format} from '@polkadot/util';
+import {BN as Bn, extractTime, formatBalance as format} from '@polkadot/util';
 
 // for million, 2 * 3-grouping + comma
 const M_LENGTH = 6 + 1;
+const DEFAULT_TIME = new Bn(6000);
 
-export function formatBalance(api, value, isShort): string {
+export function formatBalance(api, value, isShort?): string {
   const {decimals, token} = getFormat(api.registry);
   const [prefix = '', postfix = ''] = format(value, {
     decimals,
@@ -42,4 +43,37 @@ function formatDisplay(
   return `${prefix}${isShort ? '' : '.'}${
     !isShort ? ('0000' + postfix).slice(-4) : ''
   } ${unit}`;
+}
+
+export function getBlockTime(api, blockNumber) {
+  if (!blockNumber) {
+    return {
+      blockTime: DEFAULT_TIME.toNumber(),
+      timeStringParts: [],
+      formattedTime: '',
+    };
+  }
+
+  const blockTime =
+    api.consts.babe?.expectedBlockTime ||
+    api.consts.difficulty?.targetBlockTime ||
+    api.consts.timestamp?.minimumPeriod.muln(2) ||
+    DEFAULT_TIME;
+
+  const {days, hours, minutes, seconds} = extractTime(
+    Math.abs(blockTime.mul(blockNumber).toNumber()),
+  );
+
+  const timeStr = [
+    days ? (days > 1 ? `${days} days` : '1 day') : null,
+    hours ? (hours > 1 ? `${hours} hrs` : '1 hr') : null,
+    minutes ? (minutes > 1 ? `${minutes} mins` : '1 min') : null,
+    seconds ? (seconds > 1 ? `${seconds} s` : '1 s') : null,
+  ].filter((value): value is string => !!value);
+
+  return {
+    blockTime: blockTime.toNumber(),
+    timeStringParts: timeStr,
+    formattedTime: timeStr.filter(Boolean).slice(0, 2).join(' '),
+  };
 }
